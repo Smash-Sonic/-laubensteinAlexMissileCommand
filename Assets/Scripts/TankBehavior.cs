97b65d0d767f287fb6292a9c53611906a586df08
Smash-Sonic <smashsonic21@gmail.com>
2022-09-14 10:30:04 -0500

    final 5

---
+++ Assets/Scripts/TankBehavior.cs
@@ -0,0 +1,23 @@
+/*****************************************************************************
+// File Name :         TankBehavior.cs
+// Author :            Alex Laubenstein
+// Creation Date :     September 11, 2022
+//
+// Brief Description : This is a script handles barrel rotation.
+*****************************************************************************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankBehavior : MonoBehaviour
+{
+    // Update is called once per frame   
+    void Update()//moves the barrel to mouse position
+    {
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        difference.Normalize();
+        float rotation_z = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotation_z + 270);
+        //Hello World
+    }
+}
